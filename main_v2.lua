c17a8f538946a2e927df289f5567f81389774922
ltrottier <ludovic.trottier.2@gmail.com>
2016-12-09 22:09:45 -0500

    put back the old discriminative net

--- main_v2.lua
+++ main_v2.lua
@@ -42,6 +42,17 @@ torch.setdefaulttensortype('torch.FloatTensor')
 local DataLoader = paths.dofile('data/data.lua')
 local data = DataLoader.new(opt.nThreads, opt.dataset, opt)
 print("Dataset: " .. opt.dataset, " Size: ", data:size())
+----------------------------------------------------------------------------
+local function weights_init(m)
+   local name = torch.type(m)
+   if name:find('Convolution') then
+      m.weight:normal(0.0, 0.02)
+      m:noBias()
+   elseif name:find('BatchNormalization') then
+      if m.weight then m.weight:normal(1.0, 0.02) end
+      if m.bias then m.bias:fill(0) end
+   end
+end
 
 local nc = 3
 local nz = opt.nz
@@ -50,9 +61,35 @@ local ngf = opt.ngf
 local real_label = 1
 local fake_label = 0
 
+local SpatialBatchNormalization = nn.SpatialBatchNormalization
+local SpatialConvolution = nn.SpatialConvolution
+local SpatialFullConvolution = nn.SpatialFullConvolution
 
 local netG = createNetG(opt)
-local netD = createNetD(opt)
+-- local netD = createNetD(opt)
+
+local netD = nn.Sequential()
+
+-- input is (nc) x 64 x 64
+netD:add(SpatialConvolution(nc, ndf, 4, 4, 2, 2, 1, 1))
+netD:add(nn.LeakyReLU(0.2, true))
+-- state size: (ndf) x 32 x 32
+netD:add(SpatialConvolution(ndf, ndf * 2, 4, 4, 2, 2, 1, 1))
+netD:add(SpatialBatchNormalization(ndf * 2)):add(nn.LeakyReLU(0.2, true))
+-- state size: (ndf*2) x 16 x 16
+netD:add(SpatialConvolution(ndf * 2, ndf * 4, 4, 4, 2, 2, 1, 1))
+netD:add(SpatialBatchNormalization(ndf * 4)):add(nn.LeakyReLU(0.2, true))
+-- state size: (ndf*4) x 8 x 8
+netD:add(SpatialConvolution(ndf * 4, ndf * 8, 4, 4, 2, 2, 1, 1))
+netD:add(SpatialBatchNormalization(ndf * 8)):add(nn.LeakyReLU(0.2, true))
+-- state size: (ndf*8) x 4 x 4
+netD:add(SpatialConvolution(ndf * 8, 1, 4, 4))
+netD:add(nn.Sigmoid())
+-- state size: 1 x 1 x 1
+netD:add(nn.View(1):setNumInputDims(3))
+-- state size: 1
+
+netD:apply(weights_init)
 
 local criterion = nn.BCECriterion()
 ---------------------------------------------------------------------------
