bcb23e5fdcd82f5a429cae5e0d5dce2f00cb7854
ltrottier <ludovic.trottier.2@gmail.com>
2016-12-09 22:02:28 -0500

    modified structure

--- models/wrn-g.lua
+++ models/wrn-g.lua
@@ -19,44 +19,30 @@ local function createModel(opt)
 
    local function wide_block(nInputPlane, nOutputPlane, stride)
       local block = nn.Sequential()
-      local convs = nn.Sequential()
-
-      -- residual mapping
-      local module
-      if nInputPlane == nOutputPlane then
-        module = convs
-      else
-        module = block
-      end
-      module:add(SBatchNorm(nInputPlane))
-      module:add(ReLU(true))
       if stride == 2 then
-        convs:add(FullConvolution(nInputPlane,nOutputPlane, 3,3, 2,2, 1,1, 1,1))
+        block:add(SBatchNorm(nInputPlane))
+        block:add(ReLU(true))
+        block:add(FullConvolution(nInputPlane,nOutputPlane, 4,4, 2,2, 1,1))
       else
+        local convs = nn.Sequential()
+        convs:add(SBatchNorm(nInputPlane))
+        convs:add(ReLU(true))
         convs:add(Convolution(nInputPlane,nOutputPlane, 3,3, 1,1, 1,1))
-      end
+        convs:add(SBatchNorm(nOutputPlane))
+        convs:add(ReLU(true))
+        convs:add(Convolution(nOutputPlane,nOutputPlane, 3,3, 1,1, 1,1))
 
-      convs:add(SBatchNorm(nOutputPlane))
-      convs:add(ReLU(true))
-      convs:add(Convolution(nOutputPlane,nOutputPlane, 3,3, 1,1, 1,1))
+        local shortcut = nInputPlane == nOutputPlane
+                         and nn.Identity()
+                         or Convolution(nInputPlane,nOutputPlane, 1,1, 1,1)
 
-      -- skip connection
-      local shortcut
-      if nInputPlane == nOutputPlane then
-         shortcut = nn.Identity()
-      else
-        if stride == 2 then
-          shortcut = FullConvolution(nInputPlane,nOutputPlane, 2,2, 2,2)
-        else
-          shortcut = Convolution(nInputPlane,nOutputPlane, 1,1, 1,1)
-        end
+        block:add(nn.ConcatTable()
+                    :add(convs)
+                    :add(shortcut))
+             :add(nn.CAddTable(true))
       end
 
       return block
-         :add(nn.ConcatTable()
-            :add(convs)
-            :add(shortcut))
-         :add(nn.CAddTable(true))
    end
 
    -- Stacking Residual Units on the same stage
@@ -76,18 +62,17 @@ local function createModel(opt)
       local n = (depth - 2) / 8
       local nz = opt.nz
       local k = opt.widen_factor
-      local nStages = torch.Tensor{128*k, 64*k, 32*k, 16*k, 16}
+      local nStages = torch.Tensor{128*k, 64*k, 32*k, 16*k, 3}
 
-      model:add(FullConvolution(nz,nStages[1], 8,8)) -- one conv at the beginning (spatial size: 8x8)
-
-      model:add(layer(wide_block, nStages[1], nStages[2], n, 2)) -- Stage 1 (spatial size: 16x16)
-      model:add(layer(wide_block, nStages[2], nStages[3], n, 2)) -- Stage 2 (spatial size: 32x32)
-      model:add(layer(wide_block, nStages[3], nStages[4], n, 2)) -- Stage 3 (spatial size: 64x64)
-      model:add(layer(wide_block, nStages[4], nStages[5], n, 1)) -- Stage 3 (spatial size: 64x64)
-      model:add(SBatchNorm(nStages[5]))
-      model:add(ReLU(true))
-      model:add(Convolution(nStages[5], 3, 1,1, 1,1)) -- one conv at the end (spatial size: 64x64)
-      model:add(SBatchNorm(3))
+      model:add(FullConvolution(nz, nStages[1], 4,4)) -- one conv at the beginning (spatial size: 4x4)
+      model:add(layer(wide_block, nStages[1], nStages[2], n, 2)) -- Stage 1 (spatial size: 8x8)
+      model:add(layer(wide_block, nStages[2], nStages[3], n, 2)) -- Stage 2 (spatial size: 16x16)
+      model:add(layer(wide_block, nStages[3], nStages[4], n, 2)) -- Stage 3 (spatial size: 32x32)
+      model:add(layer(wide_block, nStages[4], nStages[5], n, 2)) -- Stage 3 (spatial size: 64x64)
+      --model:add(SBatchNorm(nStages[5]))
+      --model:add(ReLU(true))
+      --model:add(Convolution(nStages[5], 3, 1,1, 1,1)) -- one conv at the end (spatial size: 64x64)
+      --model:add(SBatchNorm(3))
       model:add(nn.Tanh())
    end
 
